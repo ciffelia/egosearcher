0f39fe0c53984fc21da6ef2e64b2dec7f8c60479
Ciffelia <mc.prince.0203@gmail.com>
2019-01-11 23:22:06 +0900

    Fix index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const egoSearcher = new EgoSearcher({
   twitter: {
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
   },
   sendGrid: process.env.SENDGRID_API_KEY
