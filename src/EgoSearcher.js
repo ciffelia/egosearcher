e243effa5f7e4b6fac7a81b47f2bfbb44bb60b7c
Ciffelia <mc.prince.0203@gmail.com>
2019-01-12 19:58:43 +0900

    ツイートの取りこぼしを(ほぼ)なくした - statuses/filter APIでリアルタイムに公開ツイートを取得 - 10秒おきにStandard search APIを叩き、公開ツイートの取りこぼしを防止 - 4時間おきにPremium Search APIを叩き、上記のいずれでも取りこぼしたツイートを拾い上げる - 70秒おきにホームライムラインからツイートを取得し、鍵垢ツイートを拾い上げる (70秒間に200ツイート以上がホームタイムラインを流れると取りこぼしの可能性あり）

--- src/EgoSearcher.js
+++ src/EgoSearcher.js
@@ -9,16 +9,84 @@ class EgoSearcher {
     this.sgMail = new MailService()
     this.sgMail.setApiKey(apiKeys.sendGrid)
 
+    this.latestFetchedTweetIdOnHomeTimeline = null
+    this.latestFetchedTweetIdOnStandardSearch = null
+    this.latestFetchedTweetIdOnPremiumSearch = null
+
+    this.notifiedTweets = new Set()
+
+    this.performHomeTimelineSearch = this.performHomeTimelineSearch.bind(this)
+    this.performStandardSearch = this.performStandardSearch.bind(this)
+    this.performPremiumSearch = this.performPremiumSearch.bind(this)
     this.handleNewTweet = this.handleNewTweet.bind(this)
   }
 
   start () {
     this.stream = this.twit.stream('statuses/filter', { track: EgoSearcher.StreamQuery })
     this.stream.on('tweet', this.handleNewTweet)
+
+    setInterval(this.performHomeTimelineSearch, 70 * 1000)
+    setInterval(this.performStandardSearch, 10 * 1000)
+    setInterval(this.performPremiumSearch, 4 * 60 * 60 * 1000)
+
+    this.performHomeTimelineSearch(true)
+    this.performStandardSearch(true)
+    this.performPremiumSearch(true)
+  }
+
+  // ホームタイムラインから検索 70秒おき
+  // 鍵ツイートを取得するために使用する
+  async performHomeTimelineSearch (isInitialExecution) {
+    const { data: results } = await this.twit.get('statuses/home_timeline', {
+      count: 200,
+      since_id: isInitialExecution ? undefined : this.latestFetchedTweetIdOnHomeTimeline
+    })
+
+    if (results.length === 0) return
+    this.latestFetchedTweetIdOnHomeTimeline = results[0].id_str
+
+    if (!isInitialExecution) this.handleNewTweets(results)
+  }
+
+  // Standard search API 10秒おき
+  async performStandardSearch (isInitialExecution) {
+    const { data: { statuses: results } } = await this.twit.get('search/tweets', {
+      q: EgoSearcher.standardSearchQuery,
+      result_type: 'recent',
+      count: 100,
+      since_id: isInitialExecution ? undefined : this.latestFetchedTweetIdOnStandardSearch
+    })
+
+    if (results.length === 0) return
+    this.latestFetchedTweetIdOnStandardSearch = results[0].id_str
+
+    if (!isInitialExecution) this.handleNewTweets(results)
+  }
+
+  // Premium search API 4時間おき
+  async performPremiumSearch (isInitialExecution) {
+    const { data: { results } } = await this.twit.get('tweets/search/30day/dev', {
+      query: EgoSearcher.premiumSearchQuery,
+      since_id: isInitialExecution ? undefined : this.latestFetchedTweetIdOnPremiumSearch
+    })
+
+    if (results.length === 0) return
+    this.latestFetchedTweetIdOnPremiumSearch = results[0].id_str
+
+    if (!isInitialExecution) this.handleNewTweets(results)
+  }
+
+  handleNewTweets (tweets) {
+    for (const tweet of tweets) {
+      this.handleNewTweet(tweet)
+    }
   }
 
   handleNewTweet (tweet) {
     if (!EgoSearcher.shouldNotify(tweet)) return
+
+    if (this.notifiedTweets.has(tweet.id_str)) return
+    this.notifiedTweets.add(tweet.id_str)
 
     const subject = `${tweet.user.name}さんが言及しました`
     const text = EgoSearcher.generateNotificationMessage(tweet)
@@ -36,6 +104,8 @@ class EgoSearcher {
     const isQuoteTweet = !!tweet.quoted_status
 
     if (isRetweet) return false
+
+    if (!tweet.text.toLowerCase().includes('ciffelia')) return false
 
     const author = tweet.user.screen_name
     const quotedUser = isQuoteTweet && tweet.quoted_status.user.screen_name
@@ -62,6 +132,8 @@ class EgoSearcher {
 
 EgoSearcher.ExcludedUserList = ['ciffelia', 'ciffelia_key', 'ciffelia_alt', 'ciffelia_nyan']
 EgoSearcher.StreamQuery = 'Ciffelia'
+EgoSearcher.standardSearchQuery = 'Ciffelia -filter:retweets -from:ciffelia -@ciffelia -from:ciffelia_key -@ciffelia_key -from:ciffelia_alt -@ciffelia_alt -from:ciffelia_nyan -@ciffelia_nyan'
+EgoSearcher.premiumSearchQuery = 'Ciffelia -from:ciffelia -@ciffelia -from:ciffelia_key -@ciffelia_key -from:ciffelia_alt -@ciffelia_alt -from:ciffelia_nyan -@ciffelia_nyan'
 
 EgoSearcher.mailFrom = {
   name: 'EgoSearcher',
